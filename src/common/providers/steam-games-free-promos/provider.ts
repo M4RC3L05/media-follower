4f127a65fd295867e95e21f25335ef7d52c93b13
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2026-05-29 20:43:19 +0100

    Fix steamdb.info fetch
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- src/common/providers/steam-games-free-promos/provider.ts
+++ src/common/providers/steam-games-free-promos/provider.ts
@@ -56,7 +56,18 @@ export class SteamGamesFreePromosProvider implements
     const txtHtml = await this.#props.httpClient.fetchText(input.url, {
       headers: {
         "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36",
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36",
+        "Accept":
+          "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Priority": "u=0, i",
+        "Pragma": "no-cache",
+        "Cache-Control": "no-cache",
       },
     });
 
